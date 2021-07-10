3e73f13762e125f1a360fc1e8035a6b1fd28face
A.Olokhtonov <a.olokhtonov@norsi-trans.ru>
2021-07-10 17:32:51 +0300

    Add gf-start-with-terminal command. Close terminal when gf2 exits. Restart the terminal on each gf-start-with-terminal call. Better /dev/pts/??? detection (but still not good)

--- gf2.cpp
+++ gf2.cpp
@@ -25,6 +25,8 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdarg.h>
 #include <dirent.h>
 #include <fcntl.h>
@@ -102,6 +104,11 @@ int currentLine;
 time_t currentFileReadTime;
 bool showingDisassembly;
 char previousLocation[256];
+
+// Terminal window:
+
+pid_t terminalPID = 0; // zero means we never tried to spawn the terminal
+char terminalPTS[256] = { 0 };
 
 // User interface:
 
@@ -439,6 +446,7 @@ const char *EvaluateExpression(const char *expression, const char *format = null
 
 extern "C" void DebuggerClose() {
 	kill(gdbPID, SIGKILL);
+	if (terminalPID != -1 && terminalPID != 0) kill(terminalPID, SIGKILL);
 	pthread_cancel(gdbThread);
 }
 
@@ -605,50 +613,69 @@ void TabCompleterRun(TabCompleter *completer, UITextbox *textbox, bool lastKeyWa
 // Commands:
 //////////////////////////////////////////////////////
 
-void SpawnTerminal(char *result)
+void SpawnTerminal()
 {
-    int terminal_pid = fork();
-    
-    if (terminal_pid == -1) {
-        exit(1);
-    } else if (terminal_pid == 0) {
-        char * const args[] = { "x-terminal-emulator", (char *) NULL };
-        execvp("x-terminal-emulator", args);
-    }
+	if (terminalPID != 0) {
+		// Fully restart the terminal. Trying to reuse the existing terminal
+		// gets tricky because it could have been closed by the user, so the zombie
+		// pid remains valid, and there is no easy way to check if terminal is a zombie (is there?)
+		kill(terminalPID, SIGINT);
+		
+		// Wait for terminal to return (so as to not leave zombie terminals, unrelated to commend above)
+		waitpid(terminalPID, 0, 0);
+		
+		terminalPID = 0;
+		memset(terminalPTS, 0, 256);
+	}
+	
+	// We need to get the pseudoterminal slave ("pts") of the terminal
+	// To get it, we:
+	// 1. Spawn a terminal (it, in turn, creates a child process for the shell)
+	// 2. Get the shells pid using "pgrep -P"
+	// 3. Get the controlling terminal from the shells pid using "ps -o %y"
+	char command[256] = { 0 };
+	char result[256] = { 0 };
+	
+	terminalPID = fork();
+	
+	if (terminalPID == -1) {
+		fprintf(stderr, "Error: terminal fork failed!\n");
+		exit(1);
+	} else if (terminalPID == 0) {
+		char * const args[] = { "x-terminal-emulator", (char *) NULL };
+		execvp("x-terminal-emulator", args);
+	}
 
-    // wait for execvp
-    // TODO: proper way to do this
-    sleep(1);
-
-    char command[256] = { 0 };
-    FILE *fp;
-
-    // get some child process of terminal (probably bash)
-    // TODO: are we sure this always works?
-    snprintf(command, 255, "pgrep -P %d", terminal_pid);
-    fp = popen(command, "r");
-    fread(result, 255, 1, fp);
-    pclose(fp);
-
-    int pid = strtod(result, NULL);
-
-    memset(command, 0, 256);
-    memset(result, 0, 256);
-    memcpy(result, "/dev/", 5);
-
-    // get controlling terminal of this child process
-    snprintf(command, 255, "ps -p %d --no-headers -o %%y", pid);
-    fp = popen(command, "r");
-    fread(result + 5, 255, 1, fp);
-    pclose(fp);
-
-    // replace the newline
-    for (int i = 0; i < 255; ++i) {
-        if (result[i] == '\n') {
-            result[i] = 0;
-            break;
-        }
-    }
+	snprintf(command, 255, "pgrep -P %d", terminalPID);
+	
+	do {
+		FILE *fp = popen(command, "r");
+		fread(result, 255, 1, fp);
+		pclose(fp);
+		usleep(10 * 1000);
+	} while (strlen(result) == 0);
+	
+	int shellPID = strtod(result, NULL);
+	
+	memset(command, 0, 256);
+	memset(result, 0, 256);
+	memcpy(terminalPTS, "/dev/", 5);
+	
+	do {
+		snprintf(command, 255, "ps -p %d --no-headers -o %%y", shellPID);
+		FILE *fp = popen(command, "r");
+		fread(terminalPTS + 5, 250, 1, fp);
+		pclose(fp);
+		usleep(10 * 1000);
+	} while (strlen(terminalPTS + 5) == 0);
+		
+	// Replace the newline
+	for (int i = 0; i < 255; ++i) {
+		if (terminalPTS[i] == '\n') {
+			terminalPTS[i] = 0;
+			break;
+		}
+	}
 }
 
 bool CommandParseInternal(const char *command, bool synchronous) {
@@ -734,12 +761,17 @@ bool CommandParseInternal(const char *command, bool synchronous) {
 			free(copy);
 			break;
 		}
-	} else if (0 == strcmp(command, "start")) {
-		char pts[256] = { 0 };
-		char modified_command[256] = { 0 };
-		SpawnTerminal(pts);
-		snprintf(modified_command, 255, "%s &> %s", command, pts);
-		DebuggerSend(modified_command, true, synchronous);
+	} else if (0 == strcmp(command, "gf-start-with-terminal")) {
+		SpawnTerminal();
+		char changeTtyCommand[256] = { 0 };
+		snprintf(changeTtyCommand, 255, "tty %s", terminalPTS);
+		DebuggerSend(changeTtyCommand, true, true);
+		DebuggerSend("start", true, synchronous);
+		
+		// Calling "start" after "tty" causes gdb to print a warning:
+		//	 "warning: GDB: Failed to set controlling terminal: Operation not permitted"
+		// This seems to be a gdb bug and "is harmless":
+		// https://github.com/microsoft/vscode-cpptools/issues/264#issuecomment-250220570
 	} else {
 		DebuggerSend(command, true, synchronous);
 		return true;
@@ -929,6 +961,7 @@ struct InterfaceWindow {
 const InterfaceCommand interfaceCommands[] = {
 	{ .label = "Run\tShift+F5", { .code = UI_KEYCODE_F5, .shift = true, .invoke = CommandSendToGDB, .cp = (void *) "r" } },
 	{ .label = "Run paused\tCtrl+F5", { .code = UI_KEYCODE_F5, .ctrl = true, .invoke = CommandSendToGDB, .cp = (void *) "start" } },
+	{ .label = "Run paused with terminal\tCtrl+Shift+F5", { .code = UI_KEYCODE_F5, .ctrl = true, .shift = true, .invoke = CommandSendToGDB, .cp = (void *) "gf-start-with-terminal" } },
 	{ .label = "Kill\tF3", { .code = UI_KEYCODE_F3, .invoke = CommandSendToGDB, .cp = (void *) "kill" } },
 	{ .label = "Restart GDB\tCtrl+R", { .code = UI_KEYCODE_LETTER('R'), .ctrl = true, .invoke = CommandSendToGDB, .cp = (void *) "gf-restart-gdb" } },
 	{ .label = "Connect\tF4", { .code = UI_KEYCODE_F4, .invoke = CommandSendToGDB, .cp = (void *) "target remote :1234" } },
