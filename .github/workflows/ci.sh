1a9fd72af667210e9245049963b92dfa83de0290
Nakst <17091026+nakst@users.noreply.github.com>
2022-01-05 15:35:20 +0000

    Create ci.sh

---
+++ .github/workflows/ci.sh
@@ -0,0 +1,17 @@
+name: CI
+
+on:
+  push:
+    branches: [ master ]
+  pull_request:
+    branches: [ master ]
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    steps:
+    - uses: actions/checkout@v2
+    - name: build
+      run: ./build.sh
