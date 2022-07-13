836bce037457a9746fef74fa0dc208629579a090
Nakst <17091026+nakst@users.noreply.github.com>
2022-07-13 21:44:14 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # Check GDB is installed and uses the expected prompt.
 gdb --version > /dev/null 2>&1 || printf "\033[0;31mWarning\033[0m: GDB not detected. You must install GDB to use gf.\n"
+gdb --version > /dev/null 2>&1 || exit 1
 echo q | gdb | grep "(gdb)" > /dev/null 2>&1 || printf "\033[0;31mWarning\033[0m: Your copy of GDB appears to be non-standard or has been heavily reconfigured with .gdbinit.\nIf you are using GDB plugins like 'GDB Dashboard' you must remove them,\nas otherwise gf will be unable to communicate with GDB.\n"
 
 # Check if FreeType is available.
