540c459b00ce8e0671a70d6fb8fff18d30ae644b
Franz Hoeltermann <francis_the_cat@gmx.de>
2025-01-13 21:52:24 +0100

    Fixed interface not opening on Hyprland

--- luigi2.h
+++ luigi2.h
@@ -5529,11 +5529,7 @@ UIWindow *UIWindowCreate(UIWindow *owner, uint32_t flags, const char *cTitle, in
 	int width = (flags & UI_WINDOW_MENU) ? 1 : _width ? _width : 800;
 	int height = (flags & UI_WINDOW_MENU) ? 1 : _height ? _height : 600;
 
-	XSetWindowAttributes attributes = {};
-	attributes.override_redirect = flags & UI_WINDOW_MENU;
-
-	window->window = XCreateWindow(ui.display, DefaultRootWindow(ui.display), 0, 0, width, height, 0, 0,
-		InputOutput, CopyFromParent, CWOverrideRedirect, &attributes);
+	window->window = XCreateSimpleWindow(ui.display, DefaultRootWindow(ui.display), 0, 0, width, height, 1, 0, 0);
 	if (cTitle) XStoreName(ui.display, window->window, cTitle);
 	XSelectInput(ui.display, window->window, SubstructureNotifyMask | ExposureMask | PointerMotionMask
 		| ButtonPressMask | ButtonReleaseMask | KeyPressMask | KeyReleaseMask | StructureNotifyMask
